7e1d623a3c44cc0920ada74241fe5c9ada22b7a3
sehee yun <seheeyun29@gmail.com>
2021-05-26 02:35:49 +0900

    📝 gitignore 수정

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,19 @@
+import { observer } from 'mobx-react';
+import React, { useEffect } from 'react';
+import startFirebaseUI from '../../firebaseInit';
+import styles from './login.module.css';
+
+const Login = props => {
+  useEffect(() => {
+    startFirebaseUI('#firebaseui');
+  }, []);
+
+  return (
+    <div className={styles.div}>
+      <p>환영 합니다!</p>
+      <div id="firebaseui"></div>
+    </div>
+  );
+};
+
+export default observer(Login);
