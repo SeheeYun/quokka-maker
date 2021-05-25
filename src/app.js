746f6211bc83c7e6005b3f798ef6db0e261236fe
sehee yun <seheeyun29@gmail.com>
2021-05-25 21:13:08 +0900

    ➕ react-router-dom 추가

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './app.css';
 import Header from './components/header/header';
+import Home from './components/home/home';
 import Login from './components/login/login';
 
 function App() {
   return (
     <>
       <Header />
-      <Login />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/home/home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = props => {
+  return <div>홈이당</div>;
+};
+
+export default Home;
