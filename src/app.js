64a13cfacba5e505aab6e5d08d4af820e9f6f2f5
sehee yun <seheeyun29@gmail.com>
2021-09-16 21:05:55 +0900

    ⚡svg preload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
-import { inject, observer } from 'mobx-react';
-import { useEffect } from 'react';
+import { observer } from 'mobx-react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './app.css';
 import CardList from './pages/cardList';
@@ -7,15 +6,7 @@ import CardMaker from './pages/cardMaker';
 import Home from './pages/home';
 import Login from './pages/login/login';
 
-function App({ authService, imgUploader, store }) {
-  useEffect(() => {
-    const images = store.moods.map(mood => mood.bgImage);
-    images.map(image => {
-      const img = new Image();
-      img.src = process.env.PUBLIC_URL + image;
-    });
-  }, []);
-
+function App({ authService, imgUploader }) {
   return (
     <>
       <BrowserRouter>
@@ -38,4 +29,4 @@ function App({ authService, imgUploader, store }) {
   );
 }
 
-export default inject('store')(observer(App));
+export default observer(App);
